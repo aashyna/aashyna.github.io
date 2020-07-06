75013df179217248dbb9907dd6be2c197d0e9c26
aashyna <66442396+aashyna@users.noreply.github.com>
2020-07-06 15:59:00 -0500

    htrx
    htrzs

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log("EeEEEEeEeeeeeEeeEEEEEeeeEEEeeeeeeeeeEEEeeeeeeeEEEE");
+var numberOfPets = 0;
+var numberOfArticles = numberOfPets * 2;
+console.log(numberOfArticles);
+alert("nothing\n \ni said nothing\n \nwhy are you reading this i said nothing\n \nyou just wasted a precious five seconds oF your life");
+var check = confirm("Do you love dogs?");
+if(check)
+{
+  alert("Welcome to the secret base of dogs!");
+}
+else
+{
+  alert("ALERT ALERT INTRUDER INTRUDER LEAVE IMMEDIATELY OR YOU WILL BE DESTROYEDDDDDDDD");
+  window.location.href="love-dogs.html"
+}
