1409e2fda01e81acd8731e6562f919ce1cb4377f
aashyna <66442396+aashyna@users.noreply.github.com>
2020-07-10 15:47:06 -0500

    The secret is hererrerererererererrerererererererrerererererererererrererererererererer
    herererererrerererererrererererererererscereterererererererersecretererererer

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-console.log("EeEEEEeEeeeeeEeeEEEEEeeeEEEeeeeeeeeeEEEeeeeeeeEEEE");
-var numberOfPets = 0;
-var numberOfArticles = numberOfPets * 2;
-console.log(numberOfArticles);
+// console.log("EeEEEEeEeeeeeEeeEEEEEeeeEEEeeeeeeeeeEEEeeeeeeeEEEE");
+// var numberOfPets = 0;
+// var numberOfArticles = numberOfPets * 2;
+// console.log(numberOfArticles);
 alert("nothing\n \ni said nothing\n \nwhy are you reading this i said nothing\n \nyou just wasted a precious five seconds oF your life");
 var check = confirm("Do you love dogs?");
 if(check)
@@ -11,5 +11,35 @@ if(check)
 else
 {
   alert("ALERT ALERT INTRUDER INTRUDER LEAVE IMMEDIATELY OR YOU WILL BE DESTROYEDDDDDDDD");
-  window.location.href="love-dogs.html"
+  window.location.href="love-dogs.html";
+}
+
+function yOURtEXThERE()
+{
+  if(typeof(Storage) !== "undefined")
+  {
+     if(localStorage.TeXtAlIeN==null)
+     {
+       while(true)
+       {
+         var secretSecretSecret = prompt("yOURtEXThERE");
+         if(secretSecretSecret==null ||secretSecretSecret=="")
+         {
+           alert("The dog gods do not aprove of this title. Please try again.");
+         }
+         else
+         {
+           break;
+         }
+       }
+       var h1IScool = document.getElementById("eAthTmLaNdCsSwItHjS");
+       h1IScool.innerHTML=secretSecretSecret;
+       localStorage.TeXtAlIeN=secretSecretSecret;
+     }
+     else
+     {
+       var h1IScool = document.getElementById("eAthTmLaNdCsSwItHjS");
+       h1IScool.innerHTML=localStorage.secretSecretSecret;
+     }
+  }
 }
