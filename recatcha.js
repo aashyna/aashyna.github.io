04dfbe8209ae509728ffe7c122e19b217bb27dd7
aashyna <66442396+aashyna@users.noreply.github.com>
2020-07-15 16:01:24 -0500

    idk
    idk
    
    why are you reading this
    
    stop reading this
    
    STOP
    
    STOP!!
    
    STOP!!!!
    
    ok you just wasted all your time

---
+++ recatcha.js
@@ -0,0 +1,8 @@
+function dogSafety()
+{
+  var recatcha = "https;//aashyna.github.io/fyguj.html?";
+  var lazy = recatcha.length;
+  var ss = window.location.href.substring(lazy);
+  console.log(ss);
+}
+dogSafety();
