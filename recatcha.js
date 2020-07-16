4caae2430622a722cb50aa2f04f45396195c06cf
aashyna <66442396+aashyna@users.noreply.github.com>
2020-07-16 15:09:46 -0500

    Update recatcha.js

--- recatcha.js
+++ recatcha.js
@@ -2,6 +2,7 @@ function dogSafety()
 {
   var recatcha = "https;//aashyna.github.io/fyguj.html?";
   var lazy = recatcha.length;
+  console.log(size);
   var ss = window.location.href.substring(lazy);
   console.log(ss);
 }
