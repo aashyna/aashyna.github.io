091505d1a61ca37c8bd1420fc52f5896878e43ef
aashyna <66442396+aashyna@users.noreply.github.com>
2020-07-22 15:42:01 -0500

    Update recatcha.js

--- recatcha.js
+++ recatcha.js
@@ -2,11 +2,8 @@ function dogSafety()
 {
   var recatcha = "https;//aashyna.github.io/fyguj.html?";
   var lazy = recatcha.length;
-  console.log(lazy);
   var ss = window.location.href.substring(lazy);
-  console.log(ss);
   var arr = ss.split("&");
-  console.log(arr);
   var signal = arr[0].split("=")[1];
   var finalSignal = signal.split("+");
   var newStuff = "";
